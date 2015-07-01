2ed7af655b3734ac7ed914819b5d0c5597f046cf
David Tsao <dtsao@users.noreply.github.com>
2015-06-30 21:47:27 -0400

    webpack is now rendering Main.js into bundle.js via babel loader.

---
+++ app/components/Main.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Main = React.createClass({
+    render: function () {
+        return (
+            <div>
+                Hello world
+            </div>
+        )
+    }
+});
+
+React.render(<Main />, document.getElementById('app'));
