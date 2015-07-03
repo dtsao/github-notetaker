b4b303f1ed18a49c56b863d6d792787ab66b6a80
David Tsao <dtsao@users.noreply.github.com>
2015-07-03 17:46:24 -0400

    Added PropTypes and added Firebase for notes (but not yet working)

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,15 +3,25 @@ var Router = require('react-router');
 var UserProfile = require('./Github/UserProfile');
 var Repos = require('./Github/Repos');
 var Notes = require('./Notes/Notes');
+var Firebase = require('Firebase');
+var ReactFireMixin = require('reactfire');
 
 var Profile = React.createClass({
-    mixins: [Router.State],
+    mixins: [Router.State, ReactFireMixin],
     getInitialState: function () {
         return {
             notes: ['note1', 'note2','note3'],
             bio: {name: 'Tom'},
             repos: [1,2,3]
         }
+    },
+    componentDidMount: function () {
+        this.ref = new Firebase('https://github-note-taker.firebaseio.com');
+        var childRef = this.ref.child(this.getParams().username);
+        this.bindAsArray(childRef, 'notes');
+    },
+    componentWillUnmount: function () {
+      this.unbind('notes');
     },
     render: function () {
         var username = this.getParams().username;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,14 @@
 var React = require('react');
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var Profile = require('../components/Profile');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 
 module.exports = (
     <Route name="app" path="/" handler={Main}>
-        <DefaultRoute handler={Home} />
+        <Route name="profile" path="profile/:username" handler={Profile}/>
+        <DefaultRoute handler={Home}/>
     </Route>
 );
