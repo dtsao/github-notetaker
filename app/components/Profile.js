64113aea3a3e3633a13f13a5dd65d4487926080c
David Tsao <dtsao@users.noreply.github.com>
2015-07-04 20:17:28 -0400

    Use axios to do http requests, returning promises, to access github user info and repos.

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -5,20 +5,21 @@ var Repos = require('./Github/Repos');
 var Notes = require('./Notes/Notes');
 var Firebase = require('Firebase');
 var ReactFireMixin = require('reactfire');
+var helpers = require('../utils/helpers');
 
 var Profile = React.createClass({
     mixins: [Router.State, ReactFireMixin],
     getInitialState: function () {
         return {
-            notes: ['this is my note', 'hello','licorice'],
-            bio: {name: 'Tom'},
-            repos: [1,2,3]
+            notes: [],
+            bio: {},
+            repos: []
         }
     },
     componentDidMount: function () {
         //this.ref = new Firebase('https://github-note-taker.firebaseio.com');
         var ref = new Firebase('burning-heat-2130.firebaseio.com');
-        ref.authAnonymously(function(error, authData) {
+        ref.authAnonymously(function (error, authData) {
             if (error) {
                 console.log("Login Failed!", error);
             } else {
@@ -39,22 +40,30 @@ var Profile = React.createClass({
         this.ref = ref;
         var childRef = this.ref.child(this.getParams().username);
         this.bindAsArray(childRef, 'notes');
+
+        helpers.getGithubInfo(this.getParams().username)
+            .then(function (dataObj) {
+                this.setState({
+                    bio: dataObj.bio,
+                    repos: dataObj.repos
+                });
+            }.bind(this));
     },
     componentWillUnmount: function () {
-      this.unbind('notes');
+        this.unbind('notes');
     },
     render: function () {
         var username = this.getParams().username;
         return (
             <div className="row">
                 <div className="col-md-4">
-                    <UserProfile username={username} bio={this.state.bio} />
+                    <UserProfile username={username} bio={this.state.bio}/>
                 </div>
                 <div className="col-md-4">
-                    <Repos username={username} repos={this.state.repos} />
+                    <Repos username={username} repos={this.state.repos}/>
                 </div>
                 <div className="col-md-4">
-                    <Notes username={username} notes={this.state.notes} />
+                    <Notes username={username} notes={this.state.notes}/>
                 </div>
             </div>
         )
