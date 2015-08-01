3d8e992358fdfff8020697f76c125fffff389625
David Tsao <dtsao@users.noreply.github.com>
2015-08-01 16:37:10 -0400

    Fixed comments in Repos.js

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -9,8 +9,11 @@ var Repos = React.createClass({
         var repos = this.props.repos.map(function (repo, index) {
             //console.log(repo.name + ' ' + index);
             return (
-                //quotes around {index} will cause the error: Warning: flattenChildren(...): Encountered two children with the same key, `.${index}`. Child keys must be unique; when two children share a key, only the first child will be used.
-                //only the first repo will be rendered with this error. Remove the quotes to get this working ocrrectly
+                // quotes around {index} i.e. key="{index}" will cause the error:
+                // Warning: flattenChildren(...): Encountered two children
+                // with the same key, `.${index}`. Child keys must be unique; when two children share a key, only the
+                // first child will be used. Only the first repo will be rendered with this error.
+                // Remove the quotes to get this working correctly.
                 <li className="list-group-item" key={index}>
                     {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
                     {repo.description && <p> {repo.description}</p>}
