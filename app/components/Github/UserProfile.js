cb3d856ebd5cb3237d79578e4e21e6878788e5cf
David Tsao <dtsao@users.noreply.github.com>
2015-08-02 09:08:43 -0400

    UserProfile - fixes #1. Also, removes unneeded span tags within li tags.

--- app/components/Github/UserProfile.js
+++ app/components/Github/UserProfile.js
@@ -1,5 +1,12 @@
 var React = require('react');
 
+function addListItem(arr, str, prefix){
+    if (str){
+        var content = prefix + ': ' + str;
+        arr.push(<li className="list-group-item" key={arr.length}>{content}</li>);
+    }
+}
+
 var UserProfile = React.createClass({
     propTypes: {
         username: React.PropTypes.string.isRequired,
@@ -7,21 +14,23 @@ var UserProfile = React.createClass({
     },
     render: function () {
         console.log(this.props.bio);
+        var items = [];
+        if (this.props.bio.avatar_url)
+            items.push(<li className="list-group-item" key={items.length}><img src={this.props.bio.avatar_url}
+                    className="img-rounded img-responsive"/></li>);
+        addListItem(items, this.props.bio.name, 'Name');
+        addListItem(items, this.props.bio.login, 'Login');
+        addListItem(items, this.props.bio.location, 'Location');
+        addListItem(items, this.props.bio.company, 'Company');
+        addListItem(items, this.props.bio.followers, 'Followers');
+        addListItem(items, this.props.bio.following, 'Following');
+        addListItem(items, this.props.bio.public_repos, 'Public Repos');
+        addListItem(items, this.props.bio.blog, 'Blog');
         return (
             <div>
                 <h3>User Profile</h3>
                 <ul className="list-group">
-                    {this.props.bio.avatar_url && <li className="list-group-item"><img src={this.props.bio.avatar_url}
-                        className="img-rounded img-responsive"/></li>}
-                    {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
-                    {this.props.bio.login && <li className="list-group-item">Login: {this.props.bio.login}</li>}
-                    {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
-                    {this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
-                    {this.props.bio.company && <li className="list-group-item">Company: {this.props.bio.company}</li>}
-                    {this.props.bio.followers && <li className="list-group-item">Followers: {this.props.bio.followers}</li>}
-                    {this.props.bio.following && <li className="list-group-item">Following: {this.props.bio.following}</li>}
-                    {this.props.bio.public_repos && <li className="list-group-item">Public Repos: {this.props.bio.public_repos}</li>}
-                    {this.props.bio.blog && <li className="list-group-item">Blog: {this.props.bio.blog}</li>}
+                    {items}
                 </ul>
             </div>
         )
